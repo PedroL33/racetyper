df1833042348616410e846be70412da35e0488fa
peter <leepeter019@gmail.com>
2020-03-20 21:38:34 -0700

    Removed redux detool and serviceworker

--- src/index.js
+++ src/index.js
@@ -2,18 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
-import { createStore, compose, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import allReducers from './reducers/index';
 import thunk from 'redux-thunk';
 import getPassages from './apiCalls/getPassages';
 
 
-const middleware = applyMiddleware(thunk );
-const devtool = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const middleware = applyMiddleware(thunk);
 
-const store = createStore(allReducers, devtool(middleware))
+const store = createStore(allReducers, middleware)
 
 store.dispatch(getPassages())
 
@@ -26,4 +24,3 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
